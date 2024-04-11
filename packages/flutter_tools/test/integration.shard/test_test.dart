62d40067bd213f026d25d0e00bceaf612de66df8
Victor Eronmosele <victoreronmosele@gmail.com>
2024-04-11 01:20:49 +0100

    Test experimental

--- packages/flutter_tools/test/integration.shard/test_test.dart
+++ packages/flutter_tools/test/integration.shard/test_test.dart
@@ -397,7 +397,7 @@ void main() {
     StreamSubscription<String>? sub;
     try {
       process = await _runFlutterTestConcurrent('trivial', automatedTestsDirectory, flutterTestDirectory,
-        extraArguments: const <String>['--start-paused', '--serve-observatory']);
+        extraArguments: const <String>['--start-paused', '--serve-observatory', '--verbose']);
       final Completer<Uri> completer = Completer<Uri>();
       final RegExp vmServiceUriRegExp = RegExp(r'((http)?:\/\/)[^\s]+');
       sub = process.stdout.transform(utf8.decoder).listen((String e) {
